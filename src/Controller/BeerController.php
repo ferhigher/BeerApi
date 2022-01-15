372b5c9b78bfa28875418a8ff6562e6cce7c90c0
FerHigher <fernandohiguero@hotmail.com>
2022-01-15 12:28:44 +0100

    BeerController

--- src/Controller/BeerController.php
+++ src/Controller/BeerController.php
@@ -2,20 +2,36 @@
 
 namespace App\Controller;
 
+use GuzzleHttp\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BeerController extends AbstractController
 {
     /**
-     * @Route("/beer", name="beer")
+     * @Route("/beers", name="beers")
      */
-    public function index(): Response
+    public function index(Request $request): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/BeerController.php',
+        $param = $request->query->get('food');
+
+        $client = new Client([
+            'base_uri' => 'https://api.punkapi.com/v2/'
         ]);
+
+        $response = $client->request('GET', 'beers', [
+            'query' => ['food' => $param]
+        ]);
+        $beers = $response->getBody()->getContents();
+
+        return new JsonResponse([
+            'response' => $beers,
+            'Status' => 'OK',
+            Response::HTTP_OK,
+        ]);
+
     }
 }
