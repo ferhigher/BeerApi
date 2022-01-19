8bd2fe1436c363e2a21f7ede5b54e69fe38710e4
FerHigher <fernandohiguero@hotmail.com>
2022-01-19 19:52:08 +0100

    unit test

--- tests/BeerTest.php
+++ tests/BeerTest.php
@@ -3,30 +3,22 @@
 namespace App\Tests;
 
 use App\Controller\GetBeersByFoodUseCase;
+use App\Infrastructure\HttpClient;
 use App\Repositories\BeerRepository;
 use PHPUnit\Framework\TestCase;
 
 class BeerTest extends TestCase
 {
-    /**
-     * @var BeerRepository
-     */
-    private $beerRepository;
-    /**
-     * @var GetBeersByFoodUseCase
-     */
-    private $getBeersByFood;
 
-    public function __construct(BeerRepository $beerRepo, GetBeersByFoodUseCase $getBeersByFood)
-    {
-        $this->beerRepository = $beerRepo;
-        $this->getBeersByFood = $getBeersByFood;
-    }
     public function testBeerFiltering(): void
     {
-        $food = 'carne_asada_with';
-        $response = $this->getBeersByFood->execute($food);
+        $client = new HttpClient();
+        $beerRepo = new BeerRepository($client);
+        $getBeersByFood = new GetBeersByFoodUseCase($beerRepo);
 
-        $this->assertTrue(true);
+        $food = 'carne_asada_with';
+        $response = $getBeersByFood->execute($food);
+
+        $this->assertEquals(3, count($response));
     }
 }
